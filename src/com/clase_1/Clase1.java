f5d27d80b37582f80333d8603ae4623d4924d0e3
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-02 22:41:56 -0300

    clase01

---
+++ src/com/clase_1/Clase1.java
@@ -0,0 +1,20 @@
+package com.clase_1;
+
+public class Clase1 {
+    public static void main(String[] args) {
+        int n = 1;
+        long l = 12312;
+        float n1 = 1.02f;
+        double n2= 1.230123d;
+        String name = "Geronimo";
+        boolean bool = true;
+
+        System.out.println(n);
+        System.out.println(l);
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(name);
+        System.out.println(bool)        ;
+
+    }
+}
